b0cb6874e73c700d8c84faa483156799f167d2c7
Parek666 <xvarga14@stud.fit.vutbr.cz>
2018-11-28 17:28:38 +0100

    do if statement

---
+++ if-generate.c
@@ -0,0 +1,5 @@
+//
+// Created by parek on 11/28/18.
+//
+
+#include "if-generate.h"
---
+++ if-generate.h
@@ -0,0 +1,8 @@
+//
+// Created by parek on 11/28/18.
+//
+
+#ifndef IFJ_IF_GENERATE_H
+#define IFJ_IF_GENERATE_H
+
+#endif //IFJ_IF_GENERATE_H
