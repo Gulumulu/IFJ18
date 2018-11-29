e9962a70c8b803b29111071edee1956f6f565dd4
root <root@localhost.localdomain>
2018-11-29 23:10:04 +0100

    generovani do dynamickeho stringu

---
+++ list.h
@@ -0,0 +1,12 @@
+//
+// Created by root on 11/29/18.
+//
+
+#ifndef IFJ_LIST_H
+#define IFJ_LIST_H
+
+extern int dyn_length; // dyn se menici delka listu
+extern int list_length; // pocitadlo delky listu
+extern char* list_str;
+
+#endif //IFJ_LIST_H
